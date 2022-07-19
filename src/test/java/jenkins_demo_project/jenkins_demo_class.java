266184a9a30efdd52aec4096880853eb32b279a7
vijay <vijay@LAPTOP-FK7QL8FC>
2022-07-19 16:28:12 +0530

    check new updates

---
+++ src/test/java/jenkins_demo_project/jenkins_demo_class.java
@@ -0,0 +1,11 @@
+package jenkins_demo_project;
+
+import org.testng.annotations.Test;
+
+public class jenkins_demo_class {
+@Test
+public void jenkinsdemo()
+{
+System.out.println("excuted jenkins");	
+}
+}
